c4cdff301ac86ce64bbb7dbf4282dcec675b1005
Michael Sampietro <mssampietro@gmail.com>
2020-04-17 15:30:37 -0300

    assets, main texts and general structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'zen-sleep';
+  readonly pillowTypes = ['preto', 'lavanda', 'branco', 'verde'];
+  images: string[] = [];
+
+  ngOnInit(): void {
+    this.images = this.pillowTypes.map((n) => `../assets/images/pillows/regular/big-${n}@2x.png`);
+    // this.images.push('../assets/images/pillows/regular/big-1@2x.png');
+    // this.images.push('../assets/images/pillows/regular/Preto-1@2x.png');
+    // this.images.push('../assets/images/pillows/regular/Preto-1@2x.png');
+    // this.images.push('../assets/images/pillows/regular/Preto-1@2x.png');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { HeaderMenuComponent } from './shared/header-menu/header-menu.component';
+import { NgbCarouselModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { HeaderMenuComponent } from './shared/header-menu/header-menu.component'
     MatToolbarModule,
     MatMenuModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    NgbCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
