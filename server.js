ce9fb4b606b638f5cbad3a456689d5e833ebe7aa
Michael Sampietro <mssampietro@gmail.com>
2020-04-23 20:15:06 -0300

    configuração heroku

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.use(express.static(__dirname + '/dist/zen-sleep'));
 
 app.listen(process.env.PORT || 8080);
 
+app.get('/teste', function(req, res) {
+    res.send('Hello World!');
+})
+
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/zen-sleep/index.html'));
 });
